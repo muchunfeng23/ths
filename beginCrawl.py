d1400d796d1883e8e01c110c2e596bad886acd94
yanglu04 <yanglu04@meicai.cn>
2018-09-13 18:23:31 +0800

    update

--- beginCrawl.py
+++ beginCrawl.py
@@ -6,8 +6,7 @@ import time,datetime
 # os.system("scrapy crawl 1_day_stastic_big_num")
 
 
-def get_week_day(date):
-    week_day_dict = {
+week_day_dict = {
         0: '星期一',
         1: '星期二',
         2: '星期三',
@@ -16,18 +15,25 @@ def get_week_day(date):
         5: '星期六',
         6: '星期天',
     }
-    day = date.weekday()
-    return week_day_dict[day]
-#
-# date = datetime.datetime.now()
-# if date.weekday == 5 or date.weekday == 6:
-#     pass
-# else:
-#     os.system("scrapy crawl 1_day_lhb")
-#     # 下面方法获取数据过多，先暂且不用
-#     # os.system("scrapy crawl 1_day_stastic_big_num")
-#     # 下面的方法目前只适合在windows下运行
-#     # os.system("scrapy crawl 1_day_share_info_everyday_use_click")
+day = datetime.datetime.now().weekday()
+print("day=",week_day_dict[day])
+if day == 5 or day == 6:
+    pass
+else:
+    os.system("scrapy crawl 1_day_lhb")
+    os.system("scrapy crawl 1_day_dfcf_hq")
+
+
+
+
+
+
+
+
+    # 下面方法获取数据过多，先暂且不用
+    # os.system("scrapy crawl 1_day_stastic_big_num")
+    # 下面的方法目前只适合在windows下运行
+    # os.system("scrapy crawl 1_day_share_info_everyday_use_click")
 
 
 # os.system("scrapy crawl kuailvCompanyOrderList")
@@ -41,8 +47,7 @@ def get_week_day(date):
 
 
 
-os.system("scrapy crawl 1_day_lhb")
-os.system("scrapy crawl 1_day_dfcf_hq")
+
 # cmdline.execute("scrapy crawl 1_day_stastic_big_num".split())
 
 
