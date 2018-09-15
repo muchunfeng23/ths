47db585280314924210c2f4a457202561b28f647
yanglu04 <yanglu04@meicai.cn>
2018-09-15 10:20:19 +0800

    update

--- beginCrawl.py
+++ beginCrawl.py
@@ -6,26 +6,26 @@ import time,datetime
 # os.system("scrapy crawl 1_day_stastic_big_num")
 
 
-week_day_dict = {
-        0: '星期一',
-        1: '星期二',
-        2: '星期三',
-        3: '星期四',
-        4: '星期五',
-        5: '星期六',
-        6: '星期天',
-    }
-day = datetime.datetime.now().weekday()
-print("day=",week_day_dict[day])
-if day == 5 or day == 6:
-    pass
-else:
-    os.system("scrapy crawl 1_day_lhb")
-    os.system("scrapy crawl 1_day_dfcf_hq")
+# week_day_dict = {
+#         0: '星期一',
+#         1: '星期二',
+#         2: '星期三',
+#         3: '星期四',
+#         4: '星期五',
+#         5: '星期六',
+#         6: '星期天',
+#     }
+# day = datetime.datetime.now().weekday()
+# print("day=",week_day_dict[day])
+# if day == 5 or day == 6:
+#     pass
+# else:
+#     os.system("scrapy crawl 1_day_lhb")
+#     os.system("scrapy crawl 1_day_dfcf_hq")
 
 
-# os.system("scrapy crawl 1_day_lhb")
-# os.system("scrapy crawl 1_day_dfcf_hq")
+os.system("scrapy crawl 1_day_lhb")
+os.system("scrapy crawl 1_day_dfcf_hq")
 
 
 
